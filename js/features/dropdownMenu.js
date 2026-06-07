61de92036c25405b0137d1ec068fa759057e2f14
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-07 17:11:29 +0200

    feat(header): add resetItems function

--- js/features/dropdownMenu.js
+++ js/features/dropdownMenu.js
@@ -1,2 +1,10 @@
 // Select all nav items that have a dropdown submenu
 const dropdownItems = document.querySelectorAll(".header__item--dropdown");
+
+// Reset transition delays on all submenu items (used when closing)
+function resetItems(item) {
+  const items = item.querySelectorAll(".header__submenu li");
+  items.forEach((li) => {
+    li.style.transitionDelay = "0s";
+  });
+}
