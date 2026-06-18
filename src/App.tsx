70a5a3cf78287200b0746acc27c415b333e4c7d1
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-18 22:12:38 +0200

    refactor: move home footer to HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import { Footer } from "./components/footer/Footer";
 
 export default function App() {
   return (
     <>
-      <RouterProvider router={router} />;
-      <Footer />;
+      <RouterProvider router={router} />
     </>
   );
 }
