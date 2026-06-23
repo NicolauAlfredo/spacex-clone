cd8674070a5cb2a7890ef5a014008b1bf65490e7
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-23 22:22:41 +0200

    feat(shop-header): create LanguageSelector component

---
+++ src/pages/shop-x/shop/components/ShopHeader/ShopNav/LanguageSelector/languages.ts
@@ -0,0 +1,191 @@
+import { getPublicAssetPath } from "../../../../../../../utils/getPublicAssetPath";
+
+export type LanguageOption = {
+  country: string;
+  label: string;
+  currencyShort: string;
+  flag: string;
+};
+
+export const languageOptions: LanguageOption[] = [
+  {
+    country: "Italy",
+    label: "Italy (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/italiano.jfif"),
+  },
+  {
+    country: "Australia",
+    label: "Australia (AUD $)",
+    currencyShort: "AUD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/australia.jfif"),
+  },
+  {
+    country: "Austria",
+    label: "Austria (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/austria.jfif"),
+  },
+  {
+    country: "Belgium",
+    label: "Belgium (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/belgio.jfif"),
+  },
+  {
+    country: "United States",
+    label: "United States (USD $)",
+    currencyShort: "USD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/america.jfif"),
+  },
+  {
+    country: "Italy",
+    label: "Italy (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/italiano.jfif"),
+  },
+  {
+    country: "Australia",
+    label: "Australia (AUD $)",
+    currencyShort: "AUD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/australia.jfif"),
+  },
+  {
+    country: "Austria",
+    label: "Austria (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/austria.jfif"),
+  },
+  {
+    country: "Belgium",
+    label: "Belgium (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/belgio.jfif"),
+  },
+  {
+    country: "United States",
+    label: "United States (USD $)",
+    currencyShort: "USD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/america.jfif"),
+  },
+  {
+    country: "Italy",
+    label: "Italy (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/italiano.jfif"),
+  },
+  {
+    country: "Australia",
+    label: "Australia (AUD $)",
+    currencyShort: "AUD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/australia.jfif"),
+  },
+  {
+    country: "Austria",
+    label: "Austria (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/austria.jfif"),
+  },
+  {
+    country: "Belgium",
+    label: "Belgium (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/belgio.jfif"),
+  },
+  {
+    country: "United States",
+    label: "United States (USD $)",
+    currencyShort: "USD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/america.jfif"),
+  },
+  {
+    country: "Italy",
+    label: "Italy (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/italiano.jfif"),
+  },
+  {
+    country: "Australia",
+    label: "Australia (AUD $)",
+    currencyShort: "AUD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/australia.jfif"),
+  },
+  {
+    country: "Austria",
+    label: "Austria (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/austria.jfif"),
+  },
+  {
+    country: "Belgium",
+    label: "Belgium (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/belgio.jfif"),
+  },
+  {
+    country: "United States",
+    label: "United States (USD $)",
+    currencyShort: "USD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/america.jfif"),
+  },
+  {
+    country: "Italy",
+    label: "Italy (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/italiano.jfif"),
+  },
+  {
+    country: "Australia",
+    label: "Australia (AUD $)",
+    currencyShort: "AUD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/australia.jfif"),
+  },
+  {
+    country: "Austria",
+    label: "Austria (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/austria.jfif"),
+  },
+  {
+    country: "Belgium",
+    label: "Belgium (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/belgio.jfif"),
+  },
+  {
+    country: "United States",
+    label: "United States (USD $)",
+    currencyShort: "USD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/america.jfif"),
+  },
+  {
+    country: "Italy",
+    label: "Italy (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/italiano.jfif"),
+  },
+  {
+    country: "Australia",
+    label: "Australia (AUD $)",
+    currencyShort: "AUD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/australia.jfif"),
+  },
+  {
+    country: "Austria",
+    label: "Austria (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/austria.jfif"),
+  },
+  {
+    country: "Belgium",
+    label: "Belgium (EUR €)",
+    currencyShort: "EUR €",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/belgio.jfif"),
+  },
+  {
+    country: "United States",
+    label: "United States (USD $)",
+    currencyShort: "USD $",
+    flag: getPublicAssetPath("assets/pages/shopx/languages/america.jfif"),
+  },
+];
