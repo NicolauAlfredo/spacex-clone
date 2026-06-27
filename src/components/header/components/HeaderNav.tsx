e1066a25c1093d03a515cb49b848cf89c2d94506
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-27 08:06:11 +0200

    refactor(header): extract HeaderNav component

---
+++ src/components/header/components/HeaderNav.tsx
@@ -0,0 +1,10 @@
+/**
+ * Renders the navigation menu.
+ *
+ * Responsibilities:
+ * - Render navigation items.
+ * - Map navigation data.
+ * - Render HeaderDropdown when needed.
+ *
+ * This component should not contain business logic.
+ */
