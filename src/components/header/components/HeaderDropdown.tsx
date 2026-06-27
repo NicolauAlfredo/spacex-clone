966e364ddfc09b16ae314095fa42390201fb7e37
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-27 08:06:32 +0200

    refactor(header): extract reusable HeaderDropdown component

---
+++ src/components/header/components/HeaderDropdown.tsx
@@ -0,0 +1,10 @@
+/**
+ * Reusable dropdown component.
+ *
+ * Responsibilities:
+ * - Render dropdown button.
+ * - Render submenu items.
+ * - Receive all data through props.
+ *
+ * Do not hardcode menu items inside this component.
+ */
