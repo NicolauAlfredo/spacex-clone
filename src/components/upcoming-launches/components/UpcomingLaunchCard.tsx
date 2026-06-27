e701b4c3aee5edbe31438211368c5bcc5b224d34
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-27 08:12:34 +0200

    refactor(upcoming-launches): extract UpcomingLaunchCard component

---
+++ src/components/upcoming-launches/components/UpcomingLaunchCard.tsx
@@ -0,0 +1,24 @@
+import type { UpcomingLaunchCardProps } from "../../../types/upcomingLaunches.types";
+import { ArrowRightIcon } from "../../header/components/icons/ArrowRightIcon";
+
+export function UpcomingLaunchCard({ launch }: UpcomingLaunchCardProps) {
+  return (
+    <article className="upcoming-launch__card">
+      <div className="upcoming-launch__image">
+        <img src={launch.imageSrc} alt={launch.imageAlt} />
+      </div>
+
+      <div className="upcoming-launch__content">
+        <span className="upcoming-launch__mission-title">
+          {launch.missionTitle}
+        </span>
+
+        <span className="upcoming-launch__countdown">{launch.countdown}</span>
+      </div>
+
+      <span className="upcoming-launch__arrow upcoming-launch__arrow--right">
+        <ArrowRightIcon />
+      </span>
+    </article>
+  );
+}
