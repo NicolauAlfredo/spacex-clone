a4a6389508a83c7af4f239e6b419ccbcd4c25b88
Rosario Floridia <rosariofloridia35@gmail.com>
2026-06-28 13:51:32 +0200

    fixed props

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,11 +1,11 @@
 import "./Content.css";
 import type { ContentProps } from "../../types/Content.types";
 
-export function SectionContent({title, description, className}: ContentProps) {
+export function SectionContent({title, description, classNameTitle, classNameText}: ContentProps) {
     return (
         <>
-                <h1 className={`section__title ${className}`}>{title}</h1>
-                <p className={`section_description ${className}`}>{description}</p>
+                <h1 className={`section__title ${classNameTitle}`}>{title}</h1>
+                <p className={`section_description ${classNameText}`}>{description}</p>
         </>
     )
 }
--- src/types/Content.types.ts
+++ src/types/Content.types.ts
@@ -1,5 +1,6 @@
 export type ContentProps = {
     title: string,
     description: string
-    className?: string
+    classNameTitle?: string
+    classNameText?: string
 }
