b1d4a93243cd2f5adf7ba41d3c559a071d0cc6f2
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-28 12:52:01 +0200

    fix(product-details): prevent horizontal overflow on mobile

--- src/pages/shop-x/shop/components/ShopHeader/ShopLogo.tsx
+++ src/pages/shop-x/shop/components/ShopHeader/ShopLogo.tsx
@@ -3,7 +3,7 @@ import { getPublicAssetPath } from "../../../../../utils/getPublicAssetPath";
 
 export function ShopLogo() {
   return (
-    <Link to="/shop" className="shop-logo" aria-label="Go to Shop homepage">
+    <Link to="/shop-x" className="shop-logo" aria-label="Go to Shop homepage">
       <img
         src={getPublicAssetPath("assets/pages/shopx/logo/xai-header-logo.webp")}
         alt="Shop xAI"
