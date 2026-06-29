41fe5e2e80bb98838e824a229619ad9810f07775
fidelevaristo <fidelevaristo7@gmail.com>
2026-06-29 23:35:40 +0200

    feat(icons): add reusable arrow right and left components

---
+++ src/pages/moon/components/MoonCarousel/icons/CarouselArrowLeft.tsx
@@ -0,0 +1,20 @@
+export function CarouselArrowLeft() {
+    return (
+        <div>
+            <svg
+                viewBox="0 0 10 6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M1 1L5 5L9 1"
+                    stroke="#FFFFFF"
+                    stroke-opacity="1"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round">
+                </path>
+            </svg>
+        </div>
+
+    )
+}
