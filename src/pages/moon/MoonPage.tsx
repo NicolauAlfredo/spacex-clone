08fa2c62ddb547ad8ff86261f88c6993d50e31e5
fidelevaristo <fidelevaristo7@gmail.com>
2026-06-29 21:57:38 +0200

    feat(moon page): add moon carousel component to moon page

--- src/pages/moon/MoonPage.tsx
+++ src/pages/moon/MoonPage.tsx
@@ -1,7 +1,10 @@
+import MoonCarousel from "./components/MoonCarousel/MoonCarousel";
+
 export function MoonPage() {
   return (
     <>
       <h1>Moon</h1>
+      <MoonCarousel />
     </>
   );
 }
