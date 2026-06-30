78e9e30b4576de3d7a4785e61af55335515be71b
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-30 10:54:35 +0200

    refactor(moon): move why section content to data layer

---
+++ src/pages/moon/components/monnWhySection/WhySection.tsx
@@ -0,0 +1,29 @@
+import { InnerText } from "./components/moonInnerText/InnerText";
+import { InfoTable } from "./components/infoTable/InfoTable";
+import { moonWhySection } from "../../../../data/moonWhySection.data";
+
+import "./WhySection.css";
+
+export function WhySection() {
+  return (
+    <section className="moon hero-section hero-section-why">
+      <div className="hero-section-why__img-wrapper">
+        <img
+          className="hero-section-why__img"
+          src={moonWhySection.image}
+          alt={moonWhySection.imageAlt}
+        />
+      </div>
+
+      <div className="hero-section-why__content">
+        <InnerText
+          className="hero-section-why"
+          title={moonWhySection.title}
+          paragraph={moonWhySection.paragraph}
+        />
+
+        <InfoTable className="hero-section-why" stats={moonWhySection.stats} />
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/moon/components/monnWhySection/components/moonInnerText/InnerText.tsx
@@ -0,0 +1,30 @@
+import type { InnerTextProps } from "../../../../../../types/innerText.types";
+
+import "./InnerText.css";
+
+export function InnerText({
+  title,
+  paragraph,
+  className,
+  buttonText,
+}: InnerTextProps) {
+  return (
+    <>
+      <h2 className={`${className}__title`}>{title.toUpperCase()}</h2>
+
+      <p className={`${className}__paragraph`}>{paragraph}</p>
+
+      {buttonText && (
+        <a className="button button--primary" href="#">
+          <span className="button__text">{buttonText}</span>
+
+          <span className="button__icon">
+            <svg width="13" height="12" viewBox="0 0 13 12" fill="none">
+              <path d="M11.9893 5.58371L12.2471 5.89914L11.9893 6.21555L8.10059 10.9782L7.3252 10.3454L10.5479 6.39914L1.39941 6.39914L1.39941 5.39914L10.5479 5.39914L7.3252 1.45383L8.10059 0.821014L11.9893 5.58371Z" />
+            </svg>
+          </span>
+        </a>
+      )}
+    </>
+  );
+}
