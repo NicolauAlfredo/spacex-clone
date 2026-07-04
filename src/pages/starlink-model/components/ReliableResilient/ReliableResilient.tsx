45734703144f5be2035bbb17e9bea0ba290b3662
Felice556 <felice.lgg@gmail.com>
2026-07-04 14:43:53 +0200

    adjusted some css

--- src/pages/starlink-model/components/ReliableResilient/ReliableResilient.tsx
+++ src/pages/starlink-model/components/ReliableResilient/ReliableResilient.tsx
@@ -1,9 +1,17 @@
-import "./ReliableResilient.css";
+
+
+import { reliableContent } from '../../../../content/reliablecontent.content';
+import { StarlinkButton } from '../StarlinkButton/StarlinkButton';
+import './ReliableResilient.css';
 
 export function ReliableResilient() {
   return (
-    <div>
-      <h1>Felice</h1>
+    <div className="starlink-section__content">
+      <h1 className="starlink-section__title">{reliableContent.title}</h1>
+      <p className="starlink-section__lable">{reliableContent.description}</p>
+      <div className="starlink-section__actions">
+        <StarlinkButton label="Learn More" href="/learn-more" variant="primary" />
+      </div>
     </div>
   );
 }
