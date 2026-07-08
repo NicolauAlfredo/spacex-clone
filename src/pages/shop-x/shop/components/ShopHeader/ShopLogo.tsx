8c740989967694950897b2de7a82c099e5a6514f
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-07-08 11:13:33 +0200

    refactor(shop-x): I adjusted the mobile menu.

--- src/pages/shop-x/shop/components/ShopHeader/ShopLogo.tsx
+++ src/pages/shop-x/shop/components/ShopHeader/ShopLogo.tsx
@@ -3,7 +3,11 @@ import { getPublicAssetPath } from "../../../../../utils/getPublicAssetPath";
 
 export function ShopLogo() {
   return (
-    <Link to="/shop-x" className="shop-logo" aria-label="Go to Shop homepage">
+    <Link
+      to="/shop-x"
+      className="shop-header__logo"
+      aria-label="Go to Shop homepage"
+    >
       <img
         src={getPublicAssetPath("assets/pages/shopx/logo/xai-header-logo.webp")}
         alt="Shop xAI"
