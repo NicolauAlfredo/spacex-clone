fec7123a29079065d32a9541a34a90be25a8931a
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-07-08 10:01:02 +0200

    chore: delete starlink old folder

--- src/pages/starlink/components/Plans/Plans.tsx
+++
@@ -1,9 +0,0 @@
-import { StarlinkResidentialButton } from "../../../starlink-model/components/Plans/components/StarlinkPlansButtons/StarlinkPlansButtons";
-
-export function Plans() {
-  return (
-    <section className="starlink-section starlink-section--plans">
-      <StarlinkResidentialButton />
-    </section>
-  );
-}
