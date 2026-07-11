1714404e693c18cbcda7ef90cee2b1e72b60cd5e
Federico Maria Calato <calatofederico48@gmail.com>
2026-07-11 15:07:53 +0200

    feat(home): add homepage content data and shared sections types

---
+++ src/types/homePage/mediaBackground.types.ts
@@ -0,0 +1,8 @@
+export type MediaBackgroundProps = {
+  mediaType: "video" | "image";
+  desktopImageSrc?: string;
+  mobileImageSrc?: string;
+  alt?: string;
+  desktopVideoSrc?: string;
+  mobileVideoSrc?: string;
+};
