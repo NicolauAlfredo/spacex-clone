8a3a5b751f9a50b0cbfd21c62aa3a3e489f87a96
Federico Maria Calato <calatofederico48@gmail.com>
2026-07-11 16:54:16 +0200

    feat(homepage): create reusable MediaBackground component

--- src/components/Section/components/MediaBackground/MediaBackground.tsx
+++ src/components/Section/components/MediaBackground/MediaBackground.tsx
@@ -1,11 +1,4 @@
-export type MediaBackgroundProps = {
-  mediaType: "video" | "image";
-  desktopImageSrc?: string;
-  mobileImageSrc?: string;
-  alt?: string;
-  desktopVideoSrc?: string;
-  mobileVideoSrc?: string;
-};
+import type { MediaBackgroundProps } from "../../../../types/homePage/mediaBackground.types";
 
 export function MediaBackground({
   mediaType,
--- src/types/homePage/mediaBackground.types.ts
+++ src/types/homePage/mediaBackground.types.ts
@@ -1,8 +1,15 @@
-export type MediaBackgroundProps = {
-  mediaType: "video" | "image";
+export type ImageBackgroundProps = {
+  mediaType: "image";
   desktopImageSrc?: string;
   mobileImageSrc?: string;
   alt?: string;
+};
+
+export type VideoBackgroundProps = {
+  mediaType: "video";
   desktopVideoSrc?: string;
   mobileVideoSrc?: string;
+  posterSrc?: string;
 };
+
+export type MediaBackgroundProps = | ImageBackgroundProps | VideoBackgroundProps;
